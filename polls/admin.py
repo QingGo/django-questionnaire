d72b4ae32f1c5e2fa3a39b07a5157bf7d9272f4a
EkiUbuntu <qinggojohn@gmail.com>
2018-07-14 00:36:53 +0800

    add models Questionnaire and change admin.py and add examples

--- polls/admin.py
+++ polls/admin.py
@@ -1,21 +1,38 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Choice, Question
+from .models import Choice, Question, Questionnaire
 
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 3
+    extra = 2
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('question_id', 'choice_text', 'votes')
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
+    list_display = ('questionnaire_id', 'question_text')
     fieldsets = [
-        (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        (None, {'fields': ['questionnaire', 'question_text']}),
     ]
     inlines = [ChoiceInline]
 
+class QuestionnaireAdmin(admin.ModelAdmin):
+    list_display = ('questionnaire_name', 'detail_info', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    fieldsets = [
+        (None, {'fields': ['questionnaire_name', 'detail_info']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [QuestionInline]
+
+
+
+admin.site.register(Questionnaire, QuestionnaireAdmin)
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+admin.site.register(Choice, ChoiceAdmin)
--- polls/models.py
+++ polls/models.py
@@ -3,12 +3,14 @@ import datetime
 from django.db import models
 from django.utils import timezone
 # Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+
+class Questionnaire(models.Model):
+    questionnaire_name = models.CharField(max_length=200)
+    detail_info = models.CharField(max_length=400)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
-        return self.question_text
+        return self.questionnaire_name
 
     def was_published_recently(self):
         now = timezone.now()
@@ -17,6 +19,15 @@ class Question(models.Model):
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'published recently'
+
+class Question(models.Model):
+    questionnaire = models.ForeignKey(Questionnaire, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.question_text
+
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
