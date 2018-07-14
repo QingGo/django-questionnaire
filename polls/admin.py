58a11851d1d7f8f2a7c85fe77920ab295a247a53
EkiUbuntu <qinggojohn@gmail.com>
2018-07-14 11:13:06 +0800

    make a admin tool to capitalize_choice

--- polls/admin.py
+++ polls/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import Choice, Question, Questionnaire
 
+def capitalize_choice(modeladmin, request, queryset):
+    print(modeladmin, request, queryset)
+    for choice in queryset:
+        choice.choice_text = choice.choice_text.capitalize()
+        choice.save()
+        
+capitalize_choice.short_description = "Make the fisrt letter of every word uppercase"
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
@@ -14,6 +21,7 @@ class QuestionInline(admin.TabularInline):
 
 class ChoiceAdmin(admin.ModelAdmin):
     list_display = ('question_id', 'choice_text', 'votes')
+    actions = [capitalize_choice]
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('questionnaire_id', 'question_text')
